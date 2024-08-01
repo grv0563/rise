11de6bdf489c9911de84a358184cd09b6fd5c857
Gaurav Gautam Shakya <grv.shakya0563@gmail.com>
2024-08-02 04:37:43 +0530

    mission and Home page content added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.css";
 import Navbar from "../../components/NavigationBar";
 import Footer from "../../components/Footer";
-import HealthcareOverview from '../../assets/Healthcare.png'
+import HealthcareOverview from "../../assets/Healthcare.png";
 function Home() {
   return (
     <div className="home-container">
@@ -14,7 +14,7 @@ function Home() {
           {" "}
           <b>What is RISE ?</b>
         </h3>
-        <p>
+        <p style={{textAlign:"center"}}>
           <b style={{ fontSize: "40px" }}>R</b>esponsive{" "}
           <b style={{ fontSize: "40px" }}>I</b>nformation{" "}
           <b style={{ fontSize: "40px" }}>S</b>haring for{" "}
@@ -42,7 +42,7 @@ function Home() {
           {" "}
           <b>Why is this so ?</b>
         </h3>
-        <p>
+        <p >
           Let's discuss first problem, first. why we have to pay-out-of-pocket.
           is because of lack of financial planning, education, income or
           something else much more bigger than these? My opinionated answer is
@@ -63,10 +63,12 @@ function Home() {
           India 60% population lives in rural area and have lower income but
           same time the Healthcare is very expensive due to high Competition and
           deficit in Healthcare professionals, large population ....and so on.
-          <h3>
-            {" "}
-            <b>Now, the Real Problem start here!</b>
-          </h3>
+        </p>
+        <h3>
+          {" "}
+          <b>Now, the Real Problem start here!</b>
+        </h3>
+        <p >
           We need Solution which gives access to funds(financial Aid) within
           justifiable duration in the circumstances where you need to pay Out of
           Pocket. <b>Do we need to invent the Wheel for this? </b>. the answer
@@ -75,22 +77,114 @@ function Home() {
           who able to see but can't use it whenever the need because of rigid,
           rough, long and unpridictable nature of system and loose thier trust
           on the system start bad mouthing the system.
-          <br></br>
-          <b>
-            So what the exiting Solution How we make usable and mitigate the
-            challenges?
-          </b>
-          <br></br>
-          Answer is <h3><b>Health Insurnace(either Private/Goverment Sponsored)</b></h3>
-          but what .. wh.. what Health Insurnace 🤔. I can understand your emotions 😊. Let me
-          make my point. To do so allow me to give you quick 64000 fit picture of Healthcare Industry and it's 
-          actors. Each actor have to maximize profit and fight for Competition. Each actor incentivize 
-          other to fight for Competition and maximize profit. These market forces create tension between
-          patient and other actors. Let's visualize-
-          
-
         </p>
-        <img src={HealthcareOverview} alt="Healthcare industry Overview"/>
+        <br></br>
+        <b>
+          So what is the exiting Solution, How we can make it more usable and
+          mitigate the challenges?
+        </b>
+        <h3><b>Answer is</b></h3>
+        
+        <h3>
+          <b>Health Insurnace(either Private or Goverment Sponsored)</b>
+        </h3>
+        <p >
+          but what .. wh.. what Health Insurnace 🤔. I can understand your
+          emotions 😊. Let me make my point. To do so allow me to give you quick
+          64000 fit picture of Healthcare Industry and it's actors. Healthcare
+          Market become complex Ecosystem with lots of actors. Each actor has to
+          fight for 2 forces of market:
+          <b> maximizing profit and fight for Competition</b>. Each actor
+          incentivize their Dependent(supplier and Venders) actors to win the
+          Competition and maximize profit. Example - every pharma company has to
+          convince providers(Doctors) to prescribe their new drug and has to
+          negotiate with Insurnace Companies to add thier new drug in the
+          Insured Drug list. Some times theses companies can be aggresive in
+          their sales efforts which create tension among other actors primarily
+          with patient. These incentives and tensions create complex Ecosystem
+          which lead to dilemmas and Skepticism to see clearer picture and solve
+          problems. Let break this barrier and visualize 64000 fit abstract view
+          of Healthcare Industry.
+        </p>
+        <img src={HealthcareOverview} alt="Healthcare industry Overview" />
+        <p > 
+          Healthcare Industry became quite Complex over the time with many
+          actors. Let brielfy introduce each actor quicky and understand What
+          role they in Healthcare.
+          <ul>
+            <li>
+              <b>1. Patient</b>
+              <p>
+                Patients are the Consumer in the Healthcare. Whole Healthcare
+                Industry is created and runs for Patient.
+              </p>
+            </li>
+            <li>
+              <b>2. Provider</b>
+              <p>
+                Providers are Professional Service Provider firms like Hospitals
+                and clinics which provide Professional Services like
+                Health/Disease Diagnosis or prescription for Healthcare need of
+                Individual. Professional like Doctors, Nurses, technicians and
+                Allied Healthcare professionals.
+              </p>
+            </li>
+            <li>
+              <b>3. Pharma or Life Science Companies</b>
+              <p>
+                These are Companies who create Pharmaceutical Products like
+                Medicine, injection, other medical Products.
+              </p>
+            </li>
+            <li>
+              <b>4. Employer</b>
+              <p>
+                Most of the companies provide Health Benefits such as Health
+                Insurnace and Preventive Health Checkup for Employee and his
+                Dependents(family) to attract the Talent across the country and
+                globe.
+              </p>
+            </li>
+            <li>
+              <b>5. Payors</b>
+              <p>
+                Payors are very Crucial and under rated piller of any Healthcare
+                System. Payors usally comprise of 2 entitis:{" "}
+                <b>Insurnace Companies and Federal/State Government. </b>These
+                Entities provide Protection against unpridictable Healthcare
+                expediture. Most of times Cental/Federal Government Sponsored
+                Healthcare Benefits are provided via these Insurnace Companies
+                for Efficiency, effectivness and wider reach. These payors faces
+                lot of challenges which can be solved by emerging technologies
+                like<b> AI, ML and Blockchain.</b>
+              </p>
+            </li>
+            <li>
+              <b>6. Capital Market</b>
+              <p>
+                Capital Market is Indirect playor of Healthcare Industry.
+                Payors(Insurnace Companies) make profit primarily in 2 ways{" "}
+                <b>1. Underwriting 2. Capital Market </b>
+              </p>
+            </li>
+          </ul>
+        </p>
+        <h3>Back to discussion</h3>
+        <p >
+          Now, you can see the Big picture. Patient, provider and
+          Payors(Insurnace companies) are the Big Important 3. Insurance
+          Business is about taking risk to reduce other's risk of Business,
+          Machinery, Investment,falling sick ... etc. These companies have
+          strong core competency in financial Markets and uses mathematical laws
+          like "Law of large numbers" and probability to quantify and predict
+          the possibilty of number of people falling sick out of monthly/yearly
+          sold premiums. Making sure the net sum must be positive(profit).
+          Insurance works pretty fine so why we are still facing problems like
+          families go broke beacause of unexpected expediture of Healthcare. The
+          answer is these companies need to solve vital problems before
+          unlocking the full potential for insurance. Problem 1. long, Hectic
+          and Mannual Claiming process which create
+        </p>
       </div>
 
       <Footer />
--- src/pages/mission/Mission.js
+++ src/pages/mission/Mission.js
@@ -1,25 +1,86 @@
-import React from 'react'
-import Navbar from '../../components/NavigationBar';
-import './Mission.css'
-import Footer from '../../components/Footer';
+import React from "react";
+import Navbar from "../../components/NavigationBar";
+import "./Mission.css";
+import Footer from "../../components/Footer";
+import grvPhoto from "../../assets/gaurav.jpeg";
 function Mission() {
   return (
-    <div className='mission-container'>
-        <div className='mission-header'>
-        <Navbar/>
-        </div>
-        <div className='mission-main'>
-            <h2>Problem - What & Why</h2>
-            <h2>Solution is the Mission</h2>
+    <div className="mission-container">
+      <div className="mission-header">
+        <Navbar />
+      </div>
+      <div className="mission-main">
+        <h1>What is Our Mission</h1>
+        <h3>Single Portal to access the Healthcare across India </h3>
+        <h2>Problem - What & Why</h2>
+        <p>
+          We are Building Healthcare App to Address 2 Major problem of India's
+          Healthcare System.
+        </p>
+        <ul>
+          <li>
+            <b>
+              1. Presonalized Heathcare or Precision Medicine by Building system
+              of storing and sharing the Health information anytime and anywhere
+            </b>
+          </li>
+          <li>
+            <b>
+              2. Reducing Cost Of Healthcare Delivery in India by solving the
+              pain point of Healthcare insurance claim cycle delivery using
+              Balockchain and AI
+            </b>
+          </li>
+        </ul>
+        <h3></h3>
+        <h2>Solution is the Mission</h2>
+        <p>Comming soon</p>
+
+        <div className="grv-profile">
+          <div className="grv-profile-img">
             <h2>Who we Are ?</h2>
+            <img src={grvPhoto} alt="Gaurav Shakya's Profile Photo" />
+          </div>
+          <div className="grv-profile-description">
+            <p>
+              My name is Gaurav Gautam Shakya. I am an Amateur Artificial
+              intelligence Researcher, Azure Certified AI Engineer and AWS Cloud
+              Practitioner. My expertise particularly in Public Cloud Services,
+              AI and their accessibility opportunities, risks and challenges.
+              Combination of AI and Blockchain is disrupive. I love to talk
+              about disruption in Business and Technology. I believe that
+              Working on Artificial General intelligence is the most rewarding
+              career for me. I am always interested in Physics but went to
+              engineering College. where I was exposed to computer Hardware and
+              software stuff. I fall in love with the Mind-bending Complexity of
+              Computer systems. Then I was intrigued by the question "Why are
+              engineers striving for building such complex systems and Why
+              businesses and spending such big money on these systems". Now I
+              can feel the impact of these complex engineered systems which
+              empowers the individuals, SMBs, Big Business and even some of the
+              world's largest economies. I am very impressed by the people who
+              ask strategically broad questions which give me food for thought.
+              Asking or listening to broad and unconventional questions seems
+              stupid or unnecessary for the first time because of limited
+              resources but they are crucial for setting the vision and
+              existential story of a company. I am looking for someone who is
+              constantly striving to grasp the big picture of company, industry
+              or Economic opportunities over the course of time.
+            </p>
+          </div>
 
+          <div className="grv-profile-details"></div>
+          <div>
+            <a href="https://www.linkedin.com/in/gaurav0563/">Linkedin</a>
+          </div>
+
+          <a href="https://grvnotes.com/">Website</a>
         </div>
-        
+      </div>
 
-      Mission
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Mission
+export default Mission;
